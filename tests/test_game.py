802afd84425f8459226b5d7728671061b7e82779
Anders Lauvrak Espenes <andersespenes@hotmail.com>
2022-02-16 19:28:39 +0100

    Can now generate a map

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,8 +1,9 @@
 import pytest
-from game.avatar import Avatar, Inventory
-from game.enemy import Enemy
-from game.item import Item
 import unittest
+import random
+from game.avatar import Avatar, Inventory
+from game.item import Item
+from game.dungeon import Dungeon, init_layout
 
 
 class TestGame(unittest.TestCase):
@@ -16,14 +17,25 @@ class TestGame(unittest.TestCase):
 
     # avatar.move("North")
     # avatar.run()
-    @pytest.fixture
-    def my_enemy():
-        return Enemy("Test", 10, 11, 12, 13)
+    # @pytest.fixture
+    # def my_enemy():
+    #     return Enemy("Test", 10, 11, 12, 13)
 
-    def test_fight(my_avatar, my_enemy):
-        health = my_enemy.health
-        my_avatar.fight(my_enemy)
-        assert my_enemy.health < health
+    # def test_fight(my_avatar, my_enemy):
+    #     health = my_enemy.health
+    #     my_avatar.fight(my_enemy)
+    #     assert my_enemy.health < health
+
+    def test_generate_dungeon(self):
+        """Generates a random dungeon 100 times with new seed, size and nr rooms each time"""
+        dungeon = Dungeon()
+        dungeon.use_seed = False    # Generate a new dungeon each time with the same object
+        for _ in range(0, 1000):
+            dungeon._width = random.randint(12, 36)
+            dungeon._height = random.randint(12, 36)
+            dungeon.layout = init_layout(dungeon._height, dungeon._width)
+            dungeon.nr_rooms = random.randint(12, 16)
+            dungeon.create_dungeon()
 
 
 if __name__ == '__main__':
